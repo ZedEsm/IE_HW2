4aae91e4d0afa15bc2e4712d69a3c2524acb0adc
sazed <zedesm23@gmail.com>
2023-05-12 15:59:12 +0330

    change log file

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,106 +1,56 @@
-// const moment = require('moment');
-// const winston = require('winston');
-//
-// const logger = winston.createLogger({
-//     transports: [
-//         new winston.transports.Console({
-//             format: winston.format.combine(
-//                 winston.format.timestamp({
-//                     format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-//                 }),
-//                 winston.format.json()
-//             )
-//         }),
-//         new winston.transports.File({
-//             filename: __dirname + '/debug.log',
-//             format: winston.format.combine(
-//                 winston.format.timestamp({
-//                     format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-//                 }),
-//                 winston.format.json()
-//             )
-//         })
-//     ],
-//     exceptionHandlers: [
-//         new winston.transports.Console({
-//             format: winston.format.combine(
-//                 winston.format.timestamp({
-//                     format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-//                 }),
-//                 winston.format.json()
-//             )
-//         }),
-//         new winston.transports.File({
-//             filename: __dirname + '/exceptions.log',
-//             format: winston.format.combine(
-//                 winston.format.timestamp({
-//                     format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-//                 }),
-//                 winston.format.json()
-//             )
-//         })
-//     ],
-//     exitOnError: false
-// });
-//
-// module.exports = logger;
 const moment = require('moment');
 const winston = require('winston');
+
+function getTimestampFormat() {
+    return winston.format.timestamp({
+        format: () => moment().format('YYYY-MM-DD HH:mm:ss')
+    });
+}
+
+function getRequestLineFormat() {
+    return winston.format((info) => {
+        const { req } = info.metadata || {};
+        if (req) {
+            info.message += `\nRequest Line: ${req.method} ${req.originalUrl} HTTP/${req.httpVersion}`;
+        }
+        return info;
+    })();
+}
 
 const logger = winston.createLogger({
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
-                winston.format.timestamp({
-                    format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-                }),
-                winston.format.json()
-            )
+                getTimestampFormat(),
+                winston.format.json(),
+            ),
         }),
         new winston.transports.File({
             filename: __dirname + '/debug.log',
             format: winston.format.combine(
-                winston.format.timestamp({
-                    format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-                }),
+                getTimestampFormat(),
                 winston.format.json(),
-                winston.format((info) => {
-                    const { req } = info.metadata || {};
-                    if (req) {
-                        info.message += `\nRequest Line: ${req.method} ${req.originalUrl} HTTP/${req.httpVersion}`;
-                    }
-                    return info;
-                })()
-            )
-        })
+                getRequestLineFormat(),
+            ),
+        }),
     ],
     exceptionHandlers: [
         new winston.transports.Console({
             format: winston.format.combine(
-                winston.format.timestamp({
-                    format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-                }),
-                winston.format.json()
-            )
+                getTimestampFormat(),
+                winston.format.json(),
+            ),
         }),
         new winston.transports.File({
             filename: __dirname + '/exceptions.log',
             format: winston.format.combine(
-                winston.format.timestamp({
-                    format: () => moment().format('YYYY-MM-DD HH:mm:ss')
-                }),
+                getTimestampFormat(),
                 winston.format.json(),
-                winston.format((info) => {
-                    const { req } = info.metadata || {};
-                    if (req) {
-                        info.message += `\nRequest Line: ${req.method} ${req.originalUrl} HTTP/${req.httpVersion}`;
-                    }
-                    return info;
-                })()
-            )
-        })
+                getRequestLineFormat(),
+            ),
+        }),
     ],
-    exitOnError: false
+    exitOnError: false,
 });
 
 module.exports = logger;
